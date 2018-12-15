f4611a357cc9ee74b8b1bee9d45f8bae925d4495
DocCodes <evanyoung762@gmail.com>
2018-12-15 13:21:40 -0600

    :white_check_mark: :heavy_plus_sign: Tests

--- renderer/scripts.js
+++ renderer/scripts.js
@@ -110,4 +110,5 @@ function formatBytes (bytes, decimals) {
   let i = Math.floor(Math.log(bytes) / Math.log(k))
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
+module.exports = formatBytes
 // </region>
