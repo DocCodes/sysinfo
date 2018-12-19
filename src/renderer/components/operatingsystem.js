841df7e44befffd65e4f7a0e35c5f99ea207deaf
DocCodes <evanyoung762@gmail.com>
2018-12-19 15:28:17 -0600

    :art: No in-line CSS

--- src/renderer/components/operatingsystem.js
+++ src/renderer/components/operatingsystem.js
@@ -52,7 +52,7 @@ function OperatingSystem (os) {
       </div>
     </div>
     <div class="col-4 d-flex align-items-center justify-content-center">
-      <img src="images/logos/${os.logofile === '' ? 'empty' : os.logofile}.png" class="mw-100" style="max-height: 20rem;">
+      <img id="osLogo" src="images/logos/${os.logofile === '' ? 'empty' : os.logofile}.png" class="mw-100">
     </div>
   </div>
   <hr>`
